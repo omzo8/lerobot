FIX: memory leakage caused by permanent storage of decoded video frame

lerobot/common/datasets/lerobot_dataset.py
@@ -722,7 +722,7 @@ class LeRobotDataset(torch.utils.data.Dataset):
         return self.num_frames
 
     def __getitem__(self, idx) -> dict:
-        item = self.hf_dataset[idx]
+        item = deepcopy(self.hf_dataset[idx])
         ep_idx = item["episode_index"].item()
 
         query_indices = None